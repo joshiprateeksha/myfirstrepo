4d4a7b13af681cd674e3d80b874e9ee695d2ec14
joshiprateeksha <131139265+joshiprateeksha@users.noreply.github.com>
2023-04-18 15:16:12 +0530

    Update sample.java

--- .github/workflows/sample.java
+++ .github/workflows/sample.java
@@ -1,5 +1,5 @@
 class sample{
     public static void main(String[] args) {
-        System.out.println("Hello World"); 
+        System.out.println("Hello, welcome to my 1st sample workflow"); 
     }
 }
